b478124e73716b107f85fb5ea3f47a93efa8a203
apaskhaver <alexandra.paskhaver@gmail.com>
2020-12-31 10:16:20 -0500

    Format, add comments, alphabetize properties in CSS to make them easier to locate

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
+
 var level = 0;
 var started = false;
 
-//start game
+//start game, thus handles restarts
 $(document).keydown(function() {
   if (!started) {
+    //change title text and start sequence
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
@@ -15,24 +18,33 @@ $(document).keydown(function() {
 
 //record pattern user clicked, animate it, play sound
 $(".btn").click(function() {
+  //add the color user chose to an array
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
 
+  //animate and play the sound of the pressed button
   animatePress(userChosenColor);
   playSound(userChosenColor);
 
+  //check if user's choice was correct
   checkAnswer(userClickedPattern.length - 1);
 });
 
 //generate next part of pattern, flash it, play sound. Update levels.
 function nextSequence() {
+  //reset pattern user clicked to make sure they keep getting it right
   userClickedPattern = [];
+
+  //change title to update level
   $("#level-title").text("Level " + ++level);
 
+  //randomly choose next button in game pattern sequence;
+  //add to gamePattern array
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  
+
+  //animate new button in sequence to indicate it to the user; play its sound
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
 }
@@ -42,7 +54,7 @@ function playSound(name) {
   new Audio("sounds/" + name + ".mp3").play();
 }
 
-//animates button when it is pressed
+//animate buttons when pressed
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
 
@@ -55,12 +67,14 @@ function animatePress(currentColor) {
 function checkAnswer(currentLevel) {
   //if game pattern matches what the user clicked
   if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    //if at the end of the sequence, game picks next step in pattern
     if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function() {
         nextSequence();
       }, 1000);
     }
   } else {
+    //user choice is wrong
     playSound("wrong");
 
     $("body").addClass("game-over");
@@ -69,10 +83,13 @@ function checkAnswer(currentLevel) {
       $("body").removeClass("game-over");
     }, 200);
 
+    //reset everything. Now on the keydown, the whole started routine
+    //at the beginning of the game repeats on the keydown
     startOver();
   }
 }
 
+//reset all necessary variables
 function startOver() {
   level = 0;
   gamePattern = [];
